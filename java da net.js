b6f832dac85c9cbff5e752cfa6ccc70d87e75bfa
Weslley30 <76874062+Weslley30@users.noreply.github.com>
2021-06-30 11:37:19 -0300

    Add files via upload

---
+++ java da net.js
@@ -0,0 +1,8 @@
+function inf(){
+    window.open ('https://www.themoviedb.org/tv/71446-la-casa-de-papel?language=pt-BR');
+}
+
+function ver(){
+    window.open ('http://www.e-nessa-pagina-mesmo-nao-e-um-erro.com/');
+    alert("Desculpa é propriedade da Netflix");
+}
